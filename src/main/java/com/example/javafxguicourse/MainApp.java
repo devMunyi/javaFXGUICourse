07c6ed3953d3217988870b5495be525cd4038ec5
devMunyi <samunyi90@gmail.com>
2023-05-22 11:53:27 +0300

    Initial Commit

--- src/main/java/com/example/javafxguicourse/MainApp.java
+++ src/main/java/com/example/javafxguicourse/MainApp.java
@@ -8,8 +8,17 @@ import javafx.stage.Stage;
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        // add fxml
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+
+        // add stylesheet
+        // scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+        // getting css as a variable for re-usability
+        String css = this.getClass().getResource("application.css").toExternalForm();
+        scene.getStylesheets().add(css);
+
 
         stage.setTitle("Handling Events");
         stage.setScene(scene);
