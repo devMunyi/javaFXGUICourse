db97ec845425ec113736d0360736481b40155949
devMunyi <samunyi90@gmail.com>
2023-05-28 18:03:54 +0300

    Initial Commit

--- src/main/java/com/example/javafxguicourse/App.java
+++ src/main/java/com/example/javafxguicourse/App.java
@@ -21,7 +21,7 @@ public class App extends Application {
         String css = this.getClass().getResource("application.css").toExternalForm();
         scene.getStylesheets().add(css);
 
-        stage.setTitle("Using ColorPicker");
+        stage.setTitle("Using ChoiceBox");
         stage.setScene(scene);
         stage.show();
 
--- src/main/java/com/example/javafxguicourse/Scene1Controller.java
+++ src/main/java/com/example/javafxguicourse/Scene1Controller.java
@@ -3,6 +3,7 @@ package com.example.javafxguicourse;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.Node;
 import javafx.scene.image.Image;
@@ -10,26 +11,38 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Priority;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
 
-public class Scene1Controller {
+public class Scene1Controller implements Initializable {
 
-    @FXML
-    private AnchorPane myAnchorPane;
-    @FXML
-    private ColorPicker myColorPicker;
+   @FXML
+    private Label myLabel;
 
-    public void changeColor(ActionEvent event){
+   @FXML
+    private ChoiceBox<String> myChoiceBox;
+
+   private String[] food = {"Pizza", "Sushi", "Pilau"};
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        myChoiceBox.getItems().addAll(food);
+        myChoiceBox.setOnAction(this::getFood);
+    }
+
+    private void getFood(ActionEvent event) {
         try {
-            Color myColor = myColorPicker.getValue();
-            myAnchorPane.setBackground(new Background(new BackgroundFill(myColor, null,null)));
+            String myFood = String.valueOf(myChoiceBox.getValue());
+            myLabel.setText(myFood);
         }catch (Exception e){
             System.out.println(e);
         }
     }
-
 }
