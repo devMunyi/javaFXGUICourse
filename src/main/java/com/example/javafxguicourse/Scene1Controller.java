956434886f1d293e80afea2678f7bf051d834bee
devMunyi <samunyi90@gmail.com>
2023-05-30 10:29:17 +0300

    Remove unused imports

--- src/main/java/com/example/javafxguicourse/Scene1Controller.java
+++ src/main/java/com/example/javafxguicourse/Scene1Controller.java
@@ -1,26 +1,13 @@
 package com.example.javafxguicourse;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.Node;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.Priority;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
+
 
 import java.math.BigDecimal;
 import java.net.URL;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 public class Scene1Controller implements Initializable {
