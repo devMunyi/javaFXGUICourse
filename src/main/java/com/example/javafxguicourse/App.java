a22b83bbae25a46dbe26e6d09d88314424a9add3
devMunyi <samunyi90@gmail.com>
2023-05-30 12:49:08 +0300

    Initial Commit

--- src/main/java/com/example/javafxguicourse/App.java
+++ src/main/java/com/example/javafxguicourse/App.java
@@ -22,7 +22,7 @@ public class App extends Application {
             // String css = this.getClass().getResource("application.css").toExternalForm();
             // scene.getStylesheets().add(css);
 
-            stage.setTitle("Using MenuBar");
+            stage.setTitle("Using FlowPane");
             stage.setScene(scene);
             stage.show();
 
--- src/main/java/com/example/javafxguicourse/Scene1Controller.java
+++ src/main/java/com/example/javafxguicourse/Scene1Controller.java
@@ -1,19 +1,7 @@
 package com.example.javafxguicourse;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class Scene1Controller {
-
-    public void newMethod(ActionEvent event){
-        System.out.println("You selected menu item");
-    }
 
 }
