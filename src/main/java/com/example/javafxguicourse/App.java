ce9d75b73438aa3ed57d136ee634790511535be6
devMunyi <samunyi90@gmail.com>
2023-06-07 11:14:55 +0300

    Remove Unecessary Imports

--- src/main/java/com/example/javafxguicourse/App.java
+++ src/main/java/com/example/javafxguicourse/App.java
@@ -1,13 +1,11 @@
 package com.example.javafxguicourse;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
-import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class App extends Application {
